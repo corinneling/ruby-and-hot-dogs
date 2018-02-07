f516c68ef95013f320ad0ed6101b00e0dbc9d701
corinneling <corinne@heysparkbox.com>
2018-02-07 11:29:44 -0500

    add user input checks to cook class

--- customer-service/cook.rb
+++ customer-service/cook.rb
@@ -16,46 +16,64 @@ class Cook
    # welcomes the user
     hello_customer
    # calls below methods
-    first_step_to_order
-    second_step_to_order
-    third_step_to_order
-    print_order
-  end
-
-  def hello_customer
-    puts WELCOME
-  end
-
-  # asks user for input
-  def get_user_input
-    gets.chomp.to_i
-  end
-
-  # check if user input is an integer
-  def user_input_valid? some_input
-    some_input.is_a? Integer
+    first_step
   end
 
   # shows all options and stores each selection
-
-  def first_step_to_order
-    @dog_choice = dog.list_dogs[ get_user_input ]
-    user_input_valid? @dog_choice
+  def first_step
+    user_input_valid? @dog_choice = dog.list_dogs[ get_user_input ]
+    choose_y_or_n "a bun"
   end
 
-  def second_step_to_order
-    @bun_choice = bun.list_buns[ get_user_input ]
 
+  def second_step
+    user_input_valid? @bun_choice = bun.list_buns[ get_user_input ]
+    choose_y_or_n "some condiments"
   end
 
-  def third_step_to_order
-    @condiments_choice = condiments.list_condiments[ get_user_input ]
-
+  def third_step
+    user_input_valid? @condiments_choice = condiments.list_condiments[ get_user_input ]
+    print_order
   end
 
   def print_order
     order.display_final(@dog_choice, @bun_choice, @condiments_choice)
   end
 
+ # cook's helper methods
+  def hello_customer
+    puts WELCOME
+  end
+
+  # asks user for input
+  def get_user_input
+      gets.chomp.to_i
+  end
+
+  # check if user input is valid == integer && != nil
+  def user_input_valid? some_input
+    if some_input nil
+      puts "Please choose a number."
+      some_input
+    end
+  end
+
+  def choose_y_or_n which_item
+    puts "would you like to order #{which_item}?\n 1. Yes\n 2. No"
+    @this = get_user_input
+    continue_or_not
+  end
+
+  def continue_or_not
+    case @this
+    when 1
+      second_step
+    when 2
+      third_step
+    else
+      puts "I don't understand, choose 1 or 2."
+      get_user_input
+    end
+  end
 
 end
--- kitchen/ingredients.rb
+++ kitchen/ingredients.rb
@@ -8,28 +8,4 @@ class Ingredients
       puts "#{index+1}. #{option}"
     end
   end
-
-  # one method requiring at least on item be accepted: dog class will use
-  def user_input_empty? some_input
-    raise 'You need to order at least one.' unless some_input
-  end
-
-  # checks to see if the user input is a string
-  def user_input_valid? some_input
-    some_input.is_a? Integer
-  end
-  # raises an error if user inputed a string and not an integer
-  def number_needed
-    begin
-    raise ArgumentError.new("Strings don\'t work here. Please choose a number.")
-  rescue ArgumentError => e
-     puts e.message
-  end
-
-  # allows the ingredients to be optional
-  def order_item_or_skip
-    puts 'would you like to continue? y/n'
-  end
-
-end
 end
